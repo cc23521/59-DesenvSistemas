71e017ffcac7ba0a04404b39263c01d062ebf916
Gustavo Cruz <gustavoindeolcruzdev@gmail.com>
2023-03-02 14:25:38 -0300

    Atualizar solução para 2a lista de TP
    - Ajustados problemas de IMC e aproveitamento de disciplinas

--- 1oSemestre/Técnicas de Programação I/exercicios/2a-lista/imc.py
+++ 1oSemestre/Técnicas de Programação I/exercicios/2a-lista/imc.py
@@ -4,12 +4,12 @@ peso = Decimal(input("Insira um peso (kg): "))
 altura = Decimal(input("Insira uma altura (m): "))
 imc = peso / (altura ** 2)
 
-print(f"IMC é {imc:.4}")
+print(f"\nIMC é {imc:.2f}: ", end="")
 if (imc <= 18.5):
     print("Abaixo do peso normal")
-elif (imc > 18.5 and imc <= 25):
+elif (imc > 18.5) and (imc <= 25):
     print("Peso normal")
-elif (imc > 25 and imc <= 30):
+elif (imc > 25) and (imc <= 30):
     print("Acima do peso normal")
 else:
     print("Obesidade")
--- 1oSemestre/Técnicas de Programação I/exercicios/2a-lista/media.py
+++ 1oSemestre/Técnicas de Programação I/exercicios/2a-lista/media.py
@@ -1,34 +1,11 @@
-from decimal import Decimal, getcontext
+geo = float(input("Insira a nota para Geografia: "))
+port = float(input("Insira a nota para Português: "))
+mat = float(input("Insira a nota para Matemática: "))
 
-# Limita as notas a apenas uma casa decimal
-getcontext().prec = 2
+media = (geo + port + mat) / 3
+aproveitamento = round(((geo + port * 2 + mat * 3) + media) / 7, 1)
 
-# Armazena a sequência de notas para cada disciplina
-geografia = []
-matematica = []
-portugues = []
-
-# Inputa as notas para Geografia, Matemática e Português, respectivamente
-while True:
-    if len(geografia) < 3:
-        print(f"{len(geografia)} notas para Geografia inseridas")
-        geografia.append(Decimal(input("Insira a nota para Geografia: ")))
-    elif len(matematica) < 3:
-        print(f"{len(matematica)} notas para Matemática inseridas")
-        matematica.append(Decimal(input("Insira a nota para Matemática: ")))
-    elif len(portugues) < 3:
-        print(f"{len(portugues)} notas para Português inseridas")
-        portugues.append(Decimal(input("Insira a nota para Português: ")))
-    else:
-        break
-
-notaGeografia = sum(geografia)
-notaMatematica = sum(matematica)
-notaPortugues = sum(portugues)
-mediaExercicios = (notaGeografia + notaMatematica + notaPortugues) / 3
-aproveitamento = ((notaGeografia + notaMatematica * 2 + notaPortugues * 3) + mediaExercicios) / 7
-
-print(f"Média de aproveitamento é {aproveitamento}")
+print(f"\nMédia de aproveitamento é {aproveitamento}")
 print("Conceito do aluno é ", end="")
 if aproveitamento >= 9.0:
     print("A")
