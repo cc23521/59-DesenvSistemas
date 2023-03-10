e2ac8b22c3b87ecad4e71bd9b7bebb123e408538
Gustavo Cruz <gustavoindeolcruzdev@gmail.com>
2023-03-09 23:26:24 -0300

    Adicionar jogar novamente ao jogo de adivinhação

--- 1oSemestre/Técnicas de Programação I/exercicios/desafios/adivinha.py
+++ 1oSemestre/Técnicas de Programação I/exercicios/desafios/adivinha.py
@@ -4,18 +4,23 @@ from os import system
 
 inicio = 0
 fim = 50
-
 nSecreto = randint(inicio, fim)
 tentativas = 0 
 while True:
     chute = int(input("Tente um valor: "))
     if (chute == nSecreto):
+        system("clear")
         print(f"Voce acertou depois de {tentativas} tentativas")
-        exit()
+        continuar = input("Deseja jogar novamente? [y/n] ")
+        if (continuar == 'y'):
+            nSecreto = randint(inicio, fim)
+            tentativas = 0
+            continue
+        else:
+            exit()
     elif (chute < nSecreto):
         print("Muito baixo.")
     elif (chute > nSecreto):
         print("Muito alto.")
     tentativas += 1
-    system("clear")
 
