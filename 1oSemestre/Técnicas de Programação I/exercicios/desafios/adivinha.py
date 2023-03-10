66bbd1d86529a1a3969c30dd8df3802c21eac2c8
Gustavo Cruz <gustavoindeolcruzdev@gmail.com>
2023-03-10 09:42:56 -0300

    Adicionar progressão de dificuldade e limite de tentativas

--- 1oSemestre/Técnicas de Programação I/exercicios/desafios/adivinha.py
+++ 1oSemestre/Técnicas de Programação I/exercicios/desafios/adivinha.py
@@ -1,26 +1,41 @@
 from random import randint
 from sys import exit
-from os import system
+from os import system, name
+from math import log
 
-inicio = 0
-fim = 50
-nSecreto = randint(inicio, fim)
-tentativas = 0 
+verde = "\033[1;32m"
+vermelho = "\033[1;31m"
+preto = "\033[0m"
+
+# Jogo de adivinhação com escalonamento exponencial de dificuldade
+limite = 2
+nSecreto = randint(0, limite)
+tentativas = round(log(limite, 2))
+nivel = 0
 while True:
-    chute = int(input("Tente um valor: "))
+    print(f"{tentativas} tentativa(s) restante(s)")
+    chute = int(input(f"Tente um valor de 0 a {limite}: "))
     if (chute == nSecreto):
-        system("clear")
-        print(f"Voce acertou depois de {tentativas} tentativas")
-        continuar = input("Deseja jogar novamente? [y/n] ")
-        if (continuar == 'y'):
-            nSecreto = randint(inicio, fim)
-            tentativas = 0
-            continue
-        else:
+        continuar = input(f"{verde}Deseja jogar o próximo nível? [s/n] {preto}")
+        if (continuar == 'n'):
             exit()
-    elif (chute < nSecreto):
-        print("Muito baixo.")
-    elif (chute > nSecreto):
-        print("Muito alto.")
-    tentativas += 1
+        else:
+            # Inicia uma nova partida
+            limite *= 2
+            nSecreto = randint(0, limite)
+            tentativas = round(log(limite, 2))
+            nivel += 1
+            system("clear" if name == "posix" else "cls")
+            print(f"NÍVEL {nivel}")
+    else:
+        tentativas -= 1
+        if (tentativas):
+            if (chute < nSecreto):
+                print("Muito baixo.")
+            else:
+                print("Muito alto.")
+        else:
+            print(f"{vermelho}Suas chances acabaram{preto}")
+            print(f"{vermelho}O número secreto é {nSecreto}{preto}")
+            exit()
 
