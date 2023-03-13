a97d6dc699ef3d65525ba97debddcc885d366e79
Gustavo Cruz <cc23521@g.unicamp.br>
2023-03-13 19:26:41 -0300

    Modularizar reset de variáveis
    - Módulo resetaVariaveis() reseta variáveis para o início de uma
      nova partida
    - Level up agora é compulsório

--- 1oSemestre/Técnicas de Programação I/exercicios/desafios/adivinha.py
+++ 1oSemestre/Técnicas de Programação I/exercicios/desafios/adivinha.py
@@ -1,30 +1,34 @@
+"""Jogo de adivinhação numérica com escalonamento logarítmico de dificuldade."""
 from random import randint
 from sys import exit
 from os import system, name
 from math import log
 from time import sleep
 
-# Jogo de adivinhação com escalonamento exponencial de dificuldade
+def resetaVariaveis():
+    """Reinicializa variáveis para uma nova partida."""
+    global limite, nSecreto, tentativas, nivel
+    limite = 2
+    nSecreto = randint(0, limite)
+    tentativas = round(log(limite, 2))
+    nivel = 0
+
 limite = 2
 nSecreto = randint(0, limite)
 tentativas = round(log(limite, 2))
 nivel = 0
+print(f"\nNÍVEL {nivel}")
 while True:
     print(f"{tentativas} tentativa(s) restante(s)")
     chute = int(input(f"Tente um valor de 0 a {limite}: "))
     if (chute == nSecreto):
-        continuar = input(f"Deseja jogar o próximo nível? [s/n] ")
-        if (continuar == 'n'):
-            system("cls")
-            exit()
-        else:
-            # Inicia uma nova partida
-            limite *= 2
-            nSecreto = randint(0, limite)
-            tentativas = round(log(limite, 2))
-            nivel += 1
-            system("cls")
-            print(f"NÍVEL {nivel}")
+        # Inicializa variáveis para o próximo nível
+        limite *= 2
+        nSecreto = randint(0, limite)
+        tentativas = round(log(limite, 2))
+        nivel += 1
+        system("cls")
+        print(f"NÍVEL {nivel}")
     else:
         tentativas -= 1
         if (tentativas):
@@ -33,12 +37,11 @@ while True:
             else:
                 print("Muito alto.")
         else:
-            print(f"Suas chances acabaram")
-            print(f"O número secreto é {nSecreto}")
+            print("Suas chances acabaram",
+            f"O número secreto é {nSecreto}",
+            sep="\n")
             sleep(2)
-            limite = 2
-            nSecreto = randint(0, limite)
-            tentativas = round(log(limite, 2))
-            nivel = 0
+            resetaVariaveis()
             system("cls")
-            print(f"NÍVEL {nivel}")
+            print(f"\nNÍVEL {nivel}")
+
